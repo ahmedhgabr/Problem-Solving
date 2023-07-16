ebf814b9b0db43004caf558b00f1bc65242dfe6a
Ahmed Hussien <ahmedhgabr25@gmail.com>
2023-07-16 23:21:55 +0300

    Solution E

---
+++ Pupils Plan Winter 2023/Sheet1/E.java
@@ -0,0 +1,18 @@
+package Sheet1;
+
+import java.util.Scanner;
+
+public class E {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+        if (input % 2 == 0 && input >2)  {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+    }
+}
