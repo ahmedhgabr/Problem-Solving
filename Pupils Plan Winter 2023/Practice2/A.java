b14f0a169153926f86c2c4d80dbfc780ef485701
Ahmed Hussien <ahmedhgabr25@gmail.com>
2023-07-18 23:45:33 +0300

    A solution

--- Pupils Plan Winter 2023/Practice2/A.java
+++ Pupils Plan Winter 2023/Practice2/A.java
@@ -1,6 +1,6 @@
 package Practice2;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A { // A. Increasing
@@ -12,21 +12,23 @@ public class A { // A. Increasing
 
         for (int i = 0; i < n; i++) {
             int a = sc.nextInt();
-            ArrayList<Integer> input = new ArrayList<Integer>();
-            boolean isEqual = false;
+            int[] input = new int[a];
+            boolean isIncreasing = true;
 
             for (int j = 0; j < a; j++) {
-                int test = sc.nextInt();
-                if (input.contains(test)) { // check for equal values (not Increasing)
+                input[j] = sc.nextInt();
+            }
+            Arrays.sort(input);
+            for (int j = 1; j < input.length; j++) {
+                isIncreasing = input[j - 1] < input[j];
+                if (!isIncreasing) {
                     System.out.println("NO");
-                    isEqual = true;
                     break;
-                } else {
-                    input.add(test);
                 }
             }
 
-            if (!isEqual) { // no equal values means can be rearrange to be Increasing
+            if (isIncreasing) {
+
                 System.out.println("YES");
             }
 
