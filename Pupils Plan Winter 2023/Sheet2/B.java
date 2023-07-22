7b5e4440e9fa6d7bd8e67a1ed58e3e37074c2c10
Ahmed Hussien <ahmedhgabr25@gmail.com>
2023-07-22 13:42:07 +0300

    Create B.java

---
+++ Pupils Plan Winter 2023/Sheet2/B.java
@@ -0,0 +1,26 @@
+package Sheet2;
+
+import java.util.Scanner;
+
+public class B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String input = sc.next().toLowerCase();
+        boolean[] check = new boolean[26];
+        if(n < 26){
+            System.out.println("NO");
+        }
+        else{
+            for (int i = 0; i < n; i++) {
+                check[ input.charAt(i)-'a'] = true;
+            }
+            boolean output = true ;
+            for (int i = 0; i < check.length; i++) {
+                output &= check[i];
+            }
+           
+            System.out.println(output?"YES" :"NO");
+        }
+    }
+}
