52f9d9e41499d716980b2055dc8308aa31369a00
Ahmed Hussien <ahmedhgabr25@gmail.com>
2023-07-26 23:02:33 +0300

    Create D.java

---
+++ Pupils Plan Winter 2023/Sheet3/D.java
@@ -0,0 +1,36 @@
+package Sheet3;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class D {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+        Integer n = sc.nextInt();
+        Integer m = sc.nextInt();
+        int z = sc.nextInt();
+
+        sc.close();
+
+        ArrayList<Integer> calls = new ArrayList<Integer>();
+        int output = 0;
+
+        for (int i = 1; i * n <= z; i++) {
+            calls.add(i * n);
+        }
+        output = calls.size();
+        for (int i = 1; i * m <= z; i++) {
+            Integer p = i * m;
+            calls.remove(p);
+        }
+        
+        output -= calls.size();
+
+        pw.print(output);
+        pw.flush();
+    }
+
+}
