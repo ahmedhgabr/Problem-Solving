d48328b722972ae326a6df62cff03a93ae8f2748
Ahmed Hussien <ahmedhgabr25@gmail.com>
2023-07-27 22:32:23 +0300

    Create E.java

---
+++ Pupils Plan Winter 2023/Sheet3/E.java
@@ -0,0 +1,21 @@
+package Sheet3;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class E {
+    public static void main(String[] args) {
+          PrintWriter pw = new PrintWriter(System.out);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long x = sc.nextLong();
+        int count =0 ;
+        for(int i=1;i<=n;i++){
+            if(x%i==0 && x/i<=n)
+                count++;
+        }
+        pw.print(count);
+        pw.flush();
+        pw.close();
+    }
+}
