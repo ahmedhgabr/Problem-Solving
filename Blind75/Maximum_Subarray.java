072b3d1c2e9494b4f842ae0ec4041580dcc51d44
Ahmed Hussien <ahmedhgabr25@gmail.com>
2023-08-29 00:04:51 +0300

    Create Maximum_Subarray.java

---
+++ Blind75/Maximum_Subarray.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Maximum_Subarray {
+
+    public static int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            sum = Math.max(nums[i], sum + nums[i]);
+            max = Math.max(max, sum);
+        }
+        
+        return max;
+    }
+
+
+   
+}
